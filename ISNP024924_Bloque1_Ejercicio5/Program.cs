a45ceaa2ce30245164de3c609d61e2cf996f9a82
César <Csar@DESKTOP-4INIV8U>
2024-10-20 18:11:33 -0600

    Agregar archivos de proyecto.

---
+++ ISNP024924_Bloque1_Ejercicio5/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int numero;
+        int suma = 0;
+        int contador = 0;
+
+        do
+        {
+            Console.Write("Ingrese un número (0 para finalizar): ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            if (numero != 0)
+            {
+                suma += numero;
+                contador++;
+            }
+
+        } while (numero != 0);
+
+        if (contador > 0)
+        {
+            double promedio = (double)suma / contador;
+            Console.WriteLine("El promedio de los números ingresados es: " + promedio);
+        }
+        else
+        {
+            Console.WriteLine("No se ingresaron números.");
+        }
+
+        Console.WriteLine("Programa terminado.");
+    }
+}
